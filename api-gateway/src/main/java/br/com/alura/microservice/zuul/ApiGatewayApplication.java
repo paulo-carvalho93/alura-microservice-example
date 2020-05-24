dbb6d7f7997789e951b2d6b1832c3c9d033ba5e8
Paulo Carvalho <pauloej.carvalho@gmail.com>
2020-05-24 10:43:31 -0300

    Inclusão API Gateway Zuul

---
+++ api-gateway/src/main/java/br/com/alura/microservice/zuul/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package br.com.alura.microservice.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
